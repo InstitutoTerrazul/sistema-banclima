8b8de1a2e03252dfd18b8bdf3cfb306383729c2c
alisson lucas <lucasoliveira.alisson@gmail.com>
2024-03-23 19:24:10 -0300

    master | edit client complete

--- src/app/(dashboard)/(cadastro)/editar-cliente/page.jsx
+++ src/app/(dashboard)/(cadastro)/editar-cliente/page.jsx
@@ -13,6 +13,7 @@ export default function EditarCliente() {
     const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
 
     const [name, setName] = useState('');
+    const [clientId, setClientId] = useState('');
     const [cpf, setCpf] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
@@ -31,7 +32,10 @@ export default function EditarCliente() {
     const [geracaoResiduos, setGeracaoResiduos] = useState('');
     const [projeto, setProjeto] = useState('');
     const [selectedDate, setSelectedDate] = useState(null);
+    const [searchCpf, setSearchCpf] = useState('');
     const [btnText, setBtnText] = useState('Editar');
+    const [searchBtnText, setSearchBtnText] = useState('Buscar CPF');
+
 
     const [co2Emissions, setCo2Emissions] = useState('');
 
@@ -49,8 +53,72 @@ export default function EditarCliente() {
         }
     }, []);
 
+    const clearForm = () => {
+        setName('');
+        setCpf('');
+        setEmail('');
+        setPhone('');
+        setHabitantes('');
+        setProjeto('');
+        setAddress('');
+        setDate('');
+        setCodEnergia('');
+        setTitularEnergia('');
+        setCodAgua('');
+        setTitularAgua('');
+        setCodGas('');
+        setTitularGas('');
+
+    }
+
+    const handleSearchBtn = async () => {
+        setSearchBtnText('Buscando...');
+
+        const dataSearch = searchCpf
+
+
+        try {
+            const response = await fetch('http://191.252.38.35:8080/api/clientes/listarPorCpf?login=terrazul&senha=1234567', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataSearch)
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Data searched:', data[0]);
+                setClientId(data[0].id);
+                setName(data[0].nome);
+                setCpf(data[0].cpf);
+                setEmail(data[0].email);
+                setPhone(data[0].telefone);
+                setHabitantes(data[0].habitantes);
+                setAddress(data[0].endereco);
+                setDate(data[0].data);
+                setCodAgua(data[0].matriculaDeAgua);
+                setProjeto(data[0].projeto);
+                setTitularAgua(data[0].titularAguaCpf);
+                setCodEnergia(data[0].matriculaDeEnergia);
+                setTitularEnergia(data[0].titularEnergiaCpf);
+                setCodGas(data[0].matriculaDeGas);
+                setTitularGas(data[0].titularGasCpf);
+                setSearchBtnText('CPF encontrado!');
+            } else {
+                console.error('Failed to create post');
+            }
+        } catch (error) {
+            console.error('Error creating post:', error);
+        }
+
+        setTimeout(() => {
+            setSearchBtnText('Buscar CPF');
+            clearForm();
+        }, 2000);
+    }
+
     const submitForm = async () => {
-        setBtnText('Cadastrando...');
+        setBtnText('Editando...');
 
         const data = {
             nome: name,
@@ -60,7 +128,7 @@ export default function EditarCliente() {
             habitantes: habitantes,
             projeto: projeto,
             endereco: address,
-            data: selectedDate.toISOString().slice(0, 10),
+            data: date,
             matriculaDeEnergia: codEnergia,
             titularEnergiaCpf: titularEnergia,
             matriculaDeAgua: codAgua,
@@ -70,16 +138,10 @@ export default function EditarCliente() {
 
         }
 
-        const dataConsumo = {
-            consumoEnergia,
-            consumoAgua,
-            consumoGas,
-            geracaoResiduos
-        }
 
         try {
-            const response = await fetch('http://191.252.38.35:8080/api/clientes/salvar?login=terrazul&senha=1234567', {
-                method: 'POST',
+            const response = await fetch(`http://191.252.38.35:8080/api/clientes/${clientId}?login=terrazul&senha=1234567`, {
+                method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
@@ -89,7 +151,7 @@ export default function EditarCliente() {
                 // console.log('Post created:', data);
                 const data = await response.json();
                 console.log('Post created:', data);
-                setBtnText('Cadastrado!');
+                setBtnText('Editado!');
                 // localStorage.setItem('user', JSON.stringify(data));
                 // router.push('/dashboard');
             } else {
@@ -99,28 +161,11 @@ export default function EditarCliente() {
             console.error('Error creating post:', error);
         }
 
+        setTimeout(() => {
+            setSearchBtnText('Editar');
+            
+        }, 2000);
 
-        try {
-            const response = await fetch('http://191.252.38.35:8080/api/emissoes/salvar?login=terrazul&senha=1234567', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(dataConsumo)
-            });
-            if (response.ok) {
-                // console.log('Post created:', data);
-                const data = await response.json();
-                console.log('Post created:', data);
-                setBtnText('Cadastrado!');
-                // localStorage.setItem('user', JSON.stringify(data));
-                // router.push('/dashboard');
-            } else {
-                console.error('Failed to create post');
-            }
-        } catch (error) {
-            console.error('Error creating post:', error);
-        }
     }
 
     const calculateEmissions = () => {
@@ -136,8 +181,8 @@ export default function EditarCliente() {
             <h1 className="text-2xl font-bold text-gray-800 text-start">Editar Cliente</h1>
 
             <div className="flex flex-row justify-center items-center w-full gap-8 my-4">
-                <ReactInputMask required mask="999.999.999-99" maskChar="" placeholder='Digite o cpf do cliente' type="text" className="bg-white w-4/12 h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" />
-                <button type="submit" className="flex items-center justify-center bg-white text-primary px-8 py-2 rounded-lg">Buscar</button>
+                <ReactInputMask required mask="999.999.999-99" maskChar="" placeholder='Digite o cpf do cliente' type="text" className="bg-white w-4/12 h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onBlur={handleSearchBtn} value={searchCpf} onChange={(e) => setSearchCpf(e.target.value)} />
+                <button type="button" className="flex items-center justify-center bg-white text-primary px-8 py-2 rounded-lg" >{searchBtnText}</button>
             </div>
 
             <div className="flex flex-row justify-center items-center w-full gap-8">
@@ -148,21 +193,23 @@ export default function EditarCliente() {
 
 
                     <div className="flex flex-row w-full gap-4">
-                        <input type="text" placeholder="Nome completo" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setName(e.target.value)} />
+                        <input type="text" placeholder="Nome completo" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={name} onChange={(e) => setName(e.target.value)} />
                         <ReactInputMask required mask="999.999.999-99" maskChar="" placeholder='cpf' type="text" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={cpf} onChange={e => setCpf(e.target.value)} />
                     </div>
                     <div className="flex flex-row w-full gap-4">
-                        <input type="text" placeholder="Endereço" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setAddress(e.target.value)} />
+                        <input type="text" placeholder="Endereço" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={address} onChange={(e) => setAddress(e.target.value)} />
                         <DatePicker
                             selected={selectedDate}
                             onChange={handleDateChange}
                             maxDate={new Date()} // Set the maximum date to today
                             placeholderText="data"
+                            dateFormat="dd/MM/yyyy"
+                            value={selectedDate}
                             className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black"
                         />
                     </div>
                     <div className="flex flex-row w-full gap-4">
-                        <input type="text" placeholder="Email" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setEmail(e.target.value)} />
+                        <input type="text" placeholder="Email" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={email} onChange={(e) => setEmail(e.target.value)} />
                         <ReactInputMask required mask="(99)99999-9999" maskChar="" placeholder='Telefone' type="text" {...register('phone', {
                             pattern: {
                                 value: /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/,
@@ -172,7 +219,7 @@ export default function EditarCliente() {
                         })} className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={phone} onChange={e => setPhone(e.target.value)} />
                     </div>
                     <div className="flex flex-row w-full gap-4">
-                        <input type="number" placeholder="Nº de Habitantes na residência" name="" id="" className="bg-white w-1/2 h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setHabitantes(e.target.value)} />
+                        <input type="number" placeholder="Nº de Habitantes na residência" name="" id="" className="bg-white w-1/2 h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={habitantes} onChange={(e) => setHabitantes(e.target.value)} />
                         <select id="mySelect" value={projeto} onChange={(e) => setProjeto(e.target.value)} className="bg-white w-1/2 h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 text-black">
                             <option value="" disabled selected>Projeto</option>
                             <option value="Projeto 1">Projeto 1</option>
@@ -188,23 +235,23 @@ export default function EditarCliente() {
                     <h1 className="text-2xl font-bold text-gray-800">Dados contas de consumo</h1>
 
                     <div className="flex flex-row w-full gap-4">
-                        <input type="number" placeholder="Código do cliente / energia" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setCodEnergia(e.target.value)} />
-                        <input type="text" placeholder="Titular de energia do cpf" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setTitularEnergia(e.target.value)} />
+                        <input type="number" placeholder="Código do cliente / energia" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={codEnergia} onChange={(e) => setCodEnergia(e.target.value)} />
+                        <input type="text" placeholder="Titular de energia do cpf" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={titularEnergia} onChange={(e) => setTitularEnergia(e.target.value)} />
                     </div>
                     <div className="flex flex-row w-full gap-4">
-                        <input type="number" placeholder="Código do cliente / água" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setCodAgua(e.target.value)} />
-                        <input type="text" placeholder="Titular de água do cpf" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setTitularAgua(e.target.value)} />
+                        <input type="number" placeholder="Código do cliente / água" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={codAgua} onChange={(e) => setCodAgua(e.target.value)} />
+                        <input type="text" placeholder="Titular de água do cpf" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={titularAgua} onChange={(e) => setTitularAgua(e.target.value)} />
                     </div>
-                    <input type="number" placeholder="Código do cliente / gas" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setCodGas(e.target.value)} />
-                    <input type="text" placeholder="Titular de gás do cpf" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setTitularGas(e.target.value)} />
+                    <input type="number" placeholder="Código do cliente / gas" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={codGas} onChange={(e) => setCodGas(e.target.value)} />
+                    <input type="text" placeholder="Titular de gás do cpf" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" value={titularGas} onChange={(e) => setTitularGas(e.target.value)} />
 
                 </div>
             </div>
-            <div className="flex flex-row justify-center items-center w-full gap-8">
+            {/* <div className="flex flex-row justify-center items-center w-full gap-8">
                 <div className="relative flex flex-col justify-start items-start p-10 w-full gap-4 bg-white rounded-xl">
                     <div className={`absolute top-0 left-0 bg-orange-400 w-full h-4 rounded-tl-xl rounded-tr-xl`}></div>
 
-                    <h1 className="text-2xl font-bold text-gray-800">Dados de consumo - Marco zero</h1>
+                    <h1 className="text-2xl font-bold text-gray-800">Dados de consumo </h1>
 
                     <input type="number" placeholder="Consumo de energia em kWh" name="" id="" onBlur={calculateEmissions} className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setConsumoEnergia(e.target.value)} />
                     <input type="number" placeholder="Consumo de água em m³" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" onChange={(e) => setConsumoAgua(e.target.value)} />
@@ -226,10 +273,10 @@ export default function EditarCliente() {
                     <input type="text" disabled placeholder="Kg CO2e Gás nº de botijões" name="" id="" className="bg-white w-full h-11 rounded-lg focus:outline-none border border-gray-700/45 p-3 py-4 text-black" />
 
                 </div>
-            </div>
+            </div> */}
             <div className="flex flex-row justify-end items-center w-full gap-8">
-                <button type="submit" className="flex items-center justify-center bg-red-500 text-white px-8 py-2 rounded-lg">Excluir</button>
-                <button type="submit" className="flex items-center justify-center bg-white text-primary px-8 py-2 rounded-lg">{btnText}</button>
+                <button type="button" className="flex items-center justify-center bg-red-500 text-white px-8 py-2 rounded-lg">Excluir</button>
+                <button type="submit" className="flex items-center justify-center bg-white text-primary px-8 py-2 rounded-lg" onClick={submitForm}>{btnText}</button>
             </div>
         </form>
     );
