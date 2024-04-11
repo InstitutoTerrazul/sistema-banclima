34119829370da2921f67aeb5e6f474dbe07e001b
alisson-lucas <lucasolivera.alisson@gmail.com>
2024-04-11 16:43:03 -0300

    master | geting right graph data

--- src/components/AvoidedEmissionsGraph/index.jsx
+++ src/components/AvoidedEmissionsGraph/index.jsx
@@ -6,10 +6,58 @@ import { Line, Area, PolarArea, Chart as ChartJS, Bar } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 // const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 export default function AvoidedEmissionsGraph() {
+    const [emissionGraphData, setEmissionGraphData] = useState([]);
+    const [transformedData, setTransformedData] = useState({});
+
 
     useEffect(() => {
-        console.log('window', window);
+        getGraphData();
+
     }, [])
+
+    useEffect(() => {
+
+        const data = emissionGraphData;
+
+        console.log('dados crus:', data?.meses?.map(item => item.mes));
+
+        const transformedData = {
+            "mes": data?.meses?.map(item => item.mes),
+            "agua": data?.meses?.map(item => item.emissaoAgua),
+            "gas": data?.meses?.map(item => item.emissaoGas),
+            "energiaEletrica": data?.meses?.map(item => item.emissaoEnergiaEletrica),
+            "residuos": data?.meses?.map(item => item.emissaoResiduos)
+        }
+
+        console.log('dados transformados emissoes:', transformedData);
+
+        setTransformedData(transformedData);
+
+        
+    },[emissionGraphData])
+
+    const getGraphData = async () => {
+        const data = "projeto 1"
+
+        try {
+            const response = await fetch('http://191.252.38.35:8080/api/emissoesMensal/listarRelatorioSemestralEspecificoPorProjeto?login=terrazul&senha=1234567', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Grafico de historico:', data);
+                setEmissionGraphData(data);
+            } else {
+                console.error('Failed to create post');
+            }
+        } catch (error) {
+            console.error('Error creating post:', error);
+        }
+    }
 
     var options = {
         series: [{
@@ -61,31 +109,49 @@ export default function AvoidedEmissionsGraph() {
         }
     };
 
+    const options2 = {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Emissões evitadas no ultimo semestre',
+                padding: {
+                    top: 10,
+                    bottom: 10
+                },
+                
+            }
+        }
+    }
+
     const opt = {
-        labels: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        
+        // labels: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        labels: transformedData?.mes,
         datasets: [
             {
                 id: 1,
                 label: 'Água',
-                data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+                data: transformedData?.agua,
             },
             {
                 id: 2,
                 label: 'Energia',
-                data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+                data: transformedData?.energiaEletrica,
             },
             {
                 id: 3,
                 label: 'Resíduos',
-                data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+                data: transformedData?.residuos,
             },
             {
                 id: 4,
                 label: 'Gás',
-                data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+                data: transformedData?.gas,
             },
         ],
     }
+
+    console.log('meses', transformedData?.mes);
 
 
     return (
@@ -93,6 +159,7 @@ export default function AvoidedEmissionsGraph() {
             <Bar
                 datasetIdKey='id'
                 data={opt}
+                options={options2}
                 className="w-full h-96"
             />
         </>
